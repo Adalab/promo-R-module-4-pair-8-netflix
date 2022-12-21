406d1545433a27fe405e10cd1175cb20402c3f44
lauragonzalezcalvo <laura24gc@gmail.com>
2022-12-21 11:29:12 +0100

    Add the static server

--- src/index.js
+++ src/index.js
@@ -1,39 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const movies = require("./data/movies.json");
 
 // create and config server
 const server = express(); //creamos servidor, inicia el proceso de escuchar
 server.use(cors()); // configuramos el servidor para que sea una API pública, si quitamos cors será API privada
-server.use(express.json({ limit: '10Mb' })); //configuramos el servidor le decimos q vamos a trabajar con Json
+server.use(express.json({ limit: "10Mb" })); //configuramos el servidor le decimos q vamos a trabajar con Json
 
 // init express aplication
 const serverPort = 4000;
-// asociado a la línea 5 
+// asociado a la línea 5
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-// endpoint 
+// endpoint
 
-server.get('/movies', (req, res) => {
+server.get("/movies", (req, res) => {
   const response = {
-
     success: true,
-    movies: [
-      {
-        id: '1',
-        title: 'Gambita de dama',
-        gender: 'Drama',
-        image: 'https://via.placeholder.com/150'
-      },
-      {
-        id: '2',
-        title: 'Friends',
-        gender: 'Comedia',
-        image: 'https://via.placeholder.com/150'
-      }
-    ]
-
+    movies: movies.movies,
   };
   res.json(response);
 });
+
+// ruta estática
+const staticServer = "./public-react";
+server.use(express.static(staticServer));
