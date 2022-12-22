d6d2fa62d596e272cb5e58a32636b4e1dba74a31
garcia-laura <jhony@MacBook-Air.home>
2022-12-22 11:30:21 +0100

    add endpoint login

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const movies = require("./data/movies.json");
+const users = require("./data/users.json")
 
 // create and config server
 const server = express(); //creamos servidor, inicia el proceso de escuchar
@@ -17,13 +18,32 @@ server.listen(serverPort, () => {
 // endpoint
 
 server.get("/movies", (req, res) => {
+  
   const response = {
     success: true,
     movies: movies.movies,
   };
   res.json(response);
 });
+// El primer movies es la constante que elegimos entre back y front, 
+// el segundo, es la constante que hemos importado arriba
+// el tercero, es para acceder al array de objetos
+
+// endpoint login
+server.post("/login", (req, res) => {
+  const response = {
+    success: true,
+    users: users,
+  };
+  console.log(req.body);
+  res.json(response);
+});
+
 
 // ruta estática
-const staticServer = "./public-react";
+const staticServer = "./src/public-react";
 server.use(express.static(staticServer));
+
+const staticServerImage = "./src/public-movies-images";
+server.use(express.static(staticServerImage));
+
