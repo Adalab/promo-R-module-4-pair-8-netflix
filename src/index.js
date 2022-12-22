48026ca7614db12b5698ff28a40c8ef57e26c84a
Rpg87 <sra.picadillo@gmail.com>
2022-12-22 18:38:35 +0100

    add exercise 1/2/3/4 from 4.4 express JS III

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const users = require("./data/users.json")
 const server = express(); //creamos servidor, inicia el proceso de escuchar
 server.use(cors()); // configuramos el servidor para que sea una API pública, si quitamos cors será API privada
 server.use(express.json({ limit: "10Mb" })); //configuramos el servidor le decimos q vamos a trabajar con Json
+server.set('view engine', 'ejs');
 
 // init express aplication
 const serverPort = 4000;
@@ -18,6 +19,7 @@ server.listen(serverPort, () => {
 // endpoint
 
 server.get("/movies", (req, res) => {
+
 
   const response = {
     success: true,
@@ -46,21 +48,36 @@ server.post("/login", (req, res) => {
       errorMessage: "Usuaria no encontrada",
     })
   }
-  // const response = {
-
-  //   success: true,
-  //   users: users,
-  // };
-  // console.log(req.body);
-  // res.json(response);
 });
 
+// :movieID = esto es igual a URL params que en web se ve como /(movieID)=nº de ID ejemplo: movies/1
 server.get('/movies/:movieId', (req, res) => {
-  console.log('Url params:', req.params)
+  //primer movies es el importado arriba el segundo movies es el del array de movies.json
+  const foundMovie = movies.movies.find((movie) => movie.id === req.params.movieId);
+  //con esta línea estamos pintando nuestra plantilla y no es necesario poner view, lo interpreta solo
+  res.render("movie", foundMovie);
+  // console.log('Url params:', req.params)
+  res.json(foundMovie);
 
-  console.log('Url param movieId:', req.params.movieId)
+}
 
+);
+
+/* Esto es un ejemplo
+app.get("/es/film:filmId.html", (req, res) => {
+  // get film data
+  const filmData = films.find((film) => film.id === req.params.filmId);
+  console.log("film data", filmData);
+
+  // response with rendered template
+  if (filmData) {
+    res.render("pages/film", filmData);
+  } else {
+    res.render("pages/film-not-found");
+  }
 });
+*/
+
 
 // ruta estática
 const staticServer = "./src/public-react";
@@ -68,4 +85,9 @@ server.use(express.static(staticServer));
 
 const staticServerImage = "./src/public-movies-images";
 server.use(express.static(staticServerImage));
+
+
+const staticServerCss = "./src/public-styles";
+server.use(express.static(staticServerCss));
+
 
