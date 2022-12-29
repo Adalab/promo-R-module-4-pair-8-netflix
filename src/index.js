2fa98bfd9448f235de3853042e29e1f62f79a39f
lauragonzalezcalvo <laura24gc@gmail.com>
2022-12-29 09:14:35 +0100

    BBDD I exercise 4.5.5 and 4.5.6

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ const movies = require("./data/movies.json");
 const users = require("./data/users.json");
 const Database = require("better-sqlite3");
 // importación de base de datos
-// AQUÍ BASES DE DATOS I PUNTO TRES 
+// AQUÍ BASES DE DATOS I PUNTO TRES
 
-const db = new Database('./src/db/database.db', { verbose: console.log })
-const dbUser = new Database('./src/db/users.db', { verbose: console.log })
+const db = new Database("./src/db/database.db", { verbose: console.log });
+const dbUser = new Database("./src/db/users.db", { verbose: console.log });
 // create and config server
 const server = express(); //creamos servidor, inicia el proceso de escuchar
 server.use(cors()); // configuramos el servidor para que sea una API pública, si quitamos cors será API privada
 server.use(express.json({ limit: "10Mb" })); //configuramos el servidor le decimos q vamos a trabajar con Json
-server.set('view engine', 'ejs');
+server.set("view engine", "ejs");
 
 // init express aplication
 const serverPort = 4000;
@@ -21,23 +21,18 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-
-
-
-
 server.get("/movies", (req, res) => {
   //Preparamos la query.
-  const query = db.prepare(' SELECT * FROM movies ');  // Select * = seleccionamos todas las columnas de la tabla.Movies es el nombre de nuestra tabla
+  const query = db.prepare(" SELECT * FROM movies "); // Select * = seleccionamos todas las columnas de la tabla.Movies es el nombre de nuestra tabla
   //Ejecutamos la query
   const list = query.all(); //devuelve varios registros gracias al ALL
   // console.log(list);
-  console.log(req.query);
+
   const genderFilterParam = req.query.gender;
   const sortFilterParam = req.query.sort;
-  const filteredMovieGender = list
-    .filter((movie) =>
-      movie.gender.includes(genderFilterParam)
-    )
+  const filteredMovieGender = list.filter((movie) =>
+    movie.gender.includes(genderFilterParam)
+  );
   // .sort((a, b) => (a.name > b.name ? 1 : a.name < b.name ? -1 : 0));
   // sortFilterParam.sort(function (a, b) {
   //   if (a.name > b.name) {
@@ -58,38 +53,51 @@ server.get("/movies", (req, res) => {
     // movies: movies.movies// Este era nuestro json antes de utilizar el servidor de datos
   };
   res.json(response);
-
 });
 // Forma antigua de ejecutar línea 36
-// El primer movies es la constante que elegimos entre back y front, 
+// El primer movies es la constante que elegimos entre back y front,
 // el segundo, es la constante que hemos importado arriba
 // el tercero, es para acceder al array de objetos
 
 // endpoint login
 server.post("/login", (req, res) => {
-  const email = req.body.email
-  const password = req.body.password
-  const dataLogin = users.find((element) =>
-    element.email.toLowerCase() === email.toLowerCase() && element.password === password);
-  if (dataLogin) {
-    res.json({
+  const email = req.body.email;
+  const password = req.body.password;
+  //Preparamos la query.
+  const query = dbUser.prepare(
+    "SELECT * FROM users WHERE email= ? AND password =?"
+  );
+  const loginData = query.get(email, password);
+
+  // const dataLogin = users.find(
+  //   (element) =>
+  //     element.email.toLowerCase() === email.toLowerCase() &&
+  //     element.password === password
+  // );
+  if (loginData) {
+    const successLogin = {
       success: true,
-      userId: "id_de_la_usuaria_encontrada"
-    });
+      userId: loginData.id,
+    };
+    res.json(successLogin);
   } else {
-    res.json({
+    const failLogin = {
       succes: false,
       errorMessage: "Usuaria no encontrada",
-    })
+    };
+    res.json(failLogin);
   }
+  console.log(loginData);
 });
 //endpoint nueva usuaria
 server.post("/sign-up", (req, res) => {
-  const email = req.body.email
-  const password = req.body.password
-  const query = dbUser.prepare(' INSERT INTO users (email, password) VALUES (?, ?)  ');
+  const email = req.body.email;
+  const password = req.body.password;
+  const query = dbUser.prepare(
+    " INSERT INTO users (email, password) VALUES (?, ?)  "
+  );
   const result = query.run(email, password);
-  console.log(result)
+  console.log(result);
   // res.json(result);
   // res.json({
   //   "success": true,
@@ -112,17 +120,24 @@ server.post("/sign-up", (req, res) => {
 });
 
 // :movieID = esto es igual a URL params que en web se ve como /(movieID)=nº de ID ejemplo: movies/1
-server.get('/movies/:movieId', (req, res) => {
+server.get("/movies/:movieId", (req, res) => {
+  //Preparamos la query.
+  const query = db.prepare("SELECT * FROM  movies WHERE id = ?");
+  const listId = query.get(req.params.movieId);
+
+  res.render("movie", listId);
+  res.json(listId);
+
+  // -----------------------como estaba hecho antes-------------------
   //primer movies es el importado arriba el segundo movies es el del array de movies.json
-  const foundMovie = movies.movies.find((movie) => movie.id === req.params.movieId);
-  //con esta línea estamos pintando nuestra plantilla y no es necesario poner view, lo interpreta solo
-  res.render("movie", foundMovie);
-  // console.log('Url params:', req.params)
-  res.json(foundMovie);
-
-}
-
-);
+  // const foundMovie = movies.movies.find(
+  //   (movie) => movie.id === req.params.movieId
+  // );
+  // //con esta línea estamos pintando nuestra plantilla y no es necesario poner view, lo interpreta solo
+  // res.render("movie", foundMovie);
+  // // console.log('Url params:', req.params)
+  // res.json(foundMovie);
+});
 
 /* Esto es un ejemplo
 app.get("/es/film:filmId.html", (req, res) => {
@@ -146,8 +161,5 @@ server.use(express.static(staticServer));
 const staticServerImage = "./src/public-movies-images";
 server.use(express.static(staticServerImage));
 
-
 const staticServerCss = "./src/public-styles/css";
 server.use(express.static(staticServerCss));
-
-
